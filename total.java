452e5aa0564b560e729fe2154893b8aef4dc9df7
Nonthakran <152240656+Nonthakran029@users.noreply.github.com>
2024-07-25 14:30:16 +0700

    Create total.java

---
+++ total.java
@@ -0,0 +1,9 @@
+public class total {
+    public static void main(String[] args) {
+        int x, y;
+        x = 62;
+        y = 99;
+        int total = x + y;
+        System.out.println("Enternumber 62 + 99 : " +total);
+    }
+}
