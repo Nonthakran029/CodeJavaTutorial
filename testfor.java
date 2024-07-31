81f19273e09fd65d866a060234cc9ed0053d1d2b
Nonthakran <152240656+Nonthakran029@users.noreply.github.com>
2024-07-31 15:42:37 +0700

    Create testfor.java

---
+++ testfor.java
@@ -0,0 +1,9 @@
+public class testfor {
+    public static void main(String[] args) {
+        int multiplier = 2;
+        for (int i = 1; i <= 10; i++) {
+            int result = multiplier * i;
+            System.out.println(multiplier + " x " + i + " = " + result);
+        }
+    }
+}
