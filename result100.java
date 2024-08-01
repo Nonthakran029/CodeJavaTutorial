48dcf575940370c3499325621ef6070fd87c813d
Nonthakran <152240656+Nonthakran029@users.noreply.github.com>
2024-08-01 12:51:14 +0700

    Create result100.java

---
+++ result100.java
@@ -0,0 +1,11 @@
+public class result100 {
+    public static void main(String[] args) {
+        int non = 0, car = 1;
+        while (car <= 100) {
+            non += car;
+        System.out.println("car : " +car+ "non : "+non);
+        car++;
+        }
+        System.out.println("");
+    }
+}
