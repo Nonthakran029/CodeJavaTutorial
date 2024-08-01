e69dff9b4fba2c85a8c1fe2e5acb31cc1eb819d5
Nonthakran <152240656+Nonthakran029@users.noreply.github.com>
2024-08-01 10:06:49 +0700

    Create WhileLoop.java

---
+++ WhileLoop.java
@@ -0,0 +1,12 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i++){
+            System.out.println("reslut in forloop : "+i);
+        }
+        int iWhie = 1;
+        while (iWhie <= 10) {
+            System.out.println("reslut in forloop : "+iWhie);
+            iWhie += 2;
+        }
+    }
+}
