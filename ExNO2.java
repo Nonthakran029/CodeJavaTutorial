0f2e3cf57254ff7d506661ea4cc2614f05f19348
Nonthakran <152240656+Nonthakran029@users.noreply.github.com>
2024-08-01 14:05:56 +0700

    Create ExNO2.java

---
+++ ExNO2.java
@@ -0,0 +1,12 @@
+public class ExNO2 {
+    public static void main(String[] args) {
+        int limit = 100;
+        System.out.println("Numbers from 1 to " + limit + " that are divisible by both 3 and 5:");
+
+        for (int i = 1; i <= limit; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
