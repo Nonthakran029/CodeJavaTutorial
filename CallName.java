a1e77c8e8341fff2d17f37827cddf9cc6ddc3fcc
Nonthakran <152240656+Nonthakran029@users.noreply.github.com>
2024-08-02 10:09:43 +0700

    Create CallName.java

---
+++ CallName.java
@@ -0,0 +1,10 @@
+public class CallName {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i++){
+            displaymessage();
+        }
+    }
+    public static void displaymessage() {
+        System.out.println("Nonthakarn Khamnamtaeng");
+    }
+}
