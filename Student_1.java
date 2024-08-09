1c93962652d8304439cebee577754f78bb2753ae
Nonthakran <152240656+Nonthakran029@users.noreply.github.com>
2024-08-09 12:55:04 +0700

    Create Student_1.java

---
+++ Student_1.java
@@ -0,0 +1,21 @@
+public class Student_1 {
+    private String name;
+    private String email;
+
+    public Student_1(){
+        name = "Unassigned";
+        email = "Unassigned";
+    }
+    public void setName(String Student_1Name){
+        name = Student_1Name;
+    }
+    public void setEmail(String address){
+        email = address;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getEmail(){
+        return email;    
+    }
+}
